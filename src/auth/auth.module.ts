46a82672cdf73ddd705c04bc49fefb78b35d09e5
Og'abek Xurramov <xurramovogabek948@gmail.com>
2025-06-28 16:01:26 +0500

    Auth to'g'ri ishlayabdi hatolarsiz

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,24 +1,3 @@
-// import { Module } from '@nestjs/common';
-// import { AuthService } from './auth.service';
-// import { AuthController } from './auth.controller';
-// import { TypeOrmModule } from '@nestjs/typeorm';
-// import { User } from '../users/entities/user.entity';
-// import { JwtModule } from '@nestjs/jwt';
-
-// @Module({
-//   imports: [
-//     TypeOrmModule.forFeature([User]),
-//     JwtModule.register({
-//       secret: process.env.JWT_SECRET,
-//       signOptions: { expiresIn: process.env.JWT_EXPIRES_IN },
-//     }),
-//   ],
-//   controllers: [AuthController],
-//   providers: [AuthService],
-//   exports: [JwtModule],
-// })
-// export class AuthModule {}
-
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
@@ -30,7 +9,7 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
 @Module({
   imports: [
     TypeOrmModule.forFeature([User]),
-    ConfigModule, // kerak bo'ladi
+    ConfigModule,
     JwtModule.registerAsync({
       imports: [ConfigModule],
       inject: [ConfigService],
--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -2,3 +2,4 @@ export interface UserPayload {
 	id: number;
 	email: string;
   }
+  
